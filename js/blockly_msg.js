399acdec489ae63ef2a642eb8953a980d6f9639c
Andrea F. Daniele <afdaniele@ttic.edu>
2019-03-26 03:06:18 -0700

    added JS

---
+++ js/blockly_msg.js
@@ -0,0 +1,19 @@
+'use strict';
+
+goog.provide('Blockly.Msg.en');
+
+goog.require('Blockly.Msg');
+
+Blockly.Msg["CATLOGIC"] = "Logic";
+Blockly.Msg["CATLOOPS"] = "Loops";
+Blockly.Msg["CATMATH"] = "Math";
+Blockly.Msg["CATTEXT"] = "Text";
+Blockly.Msg["CATLISTS"] = "Lists";
+Blockly.Msg["CATCOLOUR"] = "Colour";
+Blockly.Msg["CATVARIABLES"] = "Variables";
+Blockly.Msg["CATFUNCTIONS"] = "Functions";
+Blockly.Msg["CATCONTROL"] = "Flow Control";
+Blockly.Msg["CATDUCKIETOWN"] = "Duckietown";
+
+Blockly.Msg["CONTROL_HUE"] = "180";
+Blockly.Msg["DUCKIETOWN_HUE"] = "0";
