5c39bde6cc6a6a220f7d3a32da1d48777f5a9378
Andrea F. Daniele <afdaniele@ttic.edu>
2019-03-26 03:03:29 -0700

    minor

--- blockly_data/blocks/control.js
+++ blockly_data/blocks/control.js
@@ -12,7 +12,7 @@ Blockly.Blocks['wait'] = {
         .appendField("seconds");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(180);
+    this.setColour("%{BKY_CONTROL_HUE}");
     this.setTooltip('');
     this.setHelpUrl('');
   }
--- blockly_data/blocks/duckietown.js
+++ blockly_data/blocks/duckietown.js
@@ -1,36 +1,9 @@
-/**
- * @license
- *
- * Copyright 2015 Erle Robotics
- * http://erlerobotics.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Blocks for Erle-Spider.
- * @author victor@erlerobot.com (Víctor Mayoral Vilches)
- */
 'use strict';
 
 goog.provide('Blockly.Blocks.duckietown');
+
 goog.require('Blockly.Blocks');
 
-
-/**
- * Common HSV hue for all blocks in this category.
- */
-Blockly.Blocks.duckietown.HUE = 0;
 
 Blockly.Blocks['move'] = {
   init: function() {
@@ -51,7 +24,7 @@ Blockly.Blocks['move'] = {
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Blocks.duckietown.HUE);
+    this.setColour("%{BKY_DUCKIETOWN_HUE}");
     this.setTooltip("Moves the Duckiebot");
     this.setHelpUrl("");
   }
@@ -92,7 +65,7 @@ Blockly.Blocks['forward'] = {
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Blocks.duckietown.HUE);
+    this.setColour("%{BKY_DUCKIETOWN_HUE}");
     this.setTooltip("Move the Duckiebot forward");
     this.setHelpUrl("");
   }
@@ -109,7 +82,7 @@ Blockly.Blocks['forward_blocks'] = {
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Blocks.duckietown.HUE);
+    this.setColour("%{BKY_DUCKIETOWN_HUE}");
     this.setTooltip("Move the Duckiebot forward");
     this.setHelpUrl("");
   }
@@ -126,7 +99,7 @@ Blockly.Blocks['backward'] = {
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Blocks.duckietown.HUE);
+    this.setColour("%{BKY_DUCKIETOWN_HUE}");
     this.setTooltip("Move the Duckiebot backward");
     this.setHelpUrl("");
   }
